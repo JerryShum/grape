76f37b2023ac5bc13322af026caeebb333e2ec0e
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-06-08 20:00:38 -0400

    Add files via upload

--- cell.py
+++ cell.py
@@ -1,94 +1,111 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, window = None):
+    def __init__(self, row, col, window=None):
+        self.__window = window
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.row = row
+        self.col = col
+        
         self.__x1 = -1
         self.__x2 = -1
         self.__y1 = -1
         self.__y2 = -1
-        self.__win = window
         
         self.visited = False
-    
-    def draw(self, x1, x2, y1, y2):
-        if self.__win is None:
-            return
-            
+   
+        
+    def draw(self,x1,y1,x2,y2):
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
         self.__y2 = y2
         
+        if self.__window == None:
+            return
+        
         if self.has_left_wall:
             point1 = Point(x1, y1)
             point2 = Point(x1, y2)
-            
             line = Line(point1, point2)
-            self.__win.draw_line(line, "black")
+            self.__window.draw_line(line, "black")
         else:
             point1 = Point(x1, y1)
             point2 = Point(x1, y2)
-            
             line = Line(point1, point2)
-            self.__win.draw_line(line, "#d9d9d9")
-            
+            self.__window.draw_line(line, "white")
+        
         if self.has_right_wall:
             point1 = Point(x2, y1)
             point2 = Point(x2, y2)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "black")
+            self.__window.draw_line(line, "black")
         else:
             point1 = Point(x2, y1)
             point2 = Point(x2, y2)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "#d9d9d9")
-
-
+            self.__window.draw_line(line, "white")
+        
         if self.has_top_wall:
             point1 = Point(x1, y1)
             point2 = Point(x2, y1)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "black")
+            self.__window.draw_line(line, "black")
         else:
             point1 = Point(x1, y1)
             point2 = Point(x2, y1)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "#d9d9d9")
-
-
+            self.__window.draw_line(line, "white")
+            
+        
         if self.has_bottom_wall:
             point1 = Point(x1, y2)
             point2 = Point(x2, y2)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "black")
+            self.__window.draw_line(line, "black")
         else:
             point1 = Point(x1, y2)
             point2 = Point(x2, y2)
-
             line = Line(point1, point2)
-            self.__win.draw_line(line, "#d9d9d9")
-    
+            self.__window.draw_line(line, "white")
+            
     def draw_move(self, to_cell, undo=False):
-        half_length = abs(self.__x2 - self.__x1) // 2
-        x_center = half_length + self.__x1
-        y_center = self.__y1 + (self.__y2 - self.__y1) // 2
-
-        half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
-        x_center2 = half_length2 + to_cell.__x1
-        y_center2 = to_cell.__y1 + (to_cell.__y2 - to_cell.__y1) // 2
-
-        fill_color = "red"
+        
+        if self.__window == None:
+            return
+        
+        #! self center coordinates
+        xoffset = abs(self.__x2 - self.__x1) // 2
+        xcenter = self.__x1 + xoffset
+        
+        yoffset = abs(self.__y2 - self.__y1) // 2
+        ycenter = self.__y1 + yoffset
+        
+        #! to_cell center coordinates
+        xoffset2 = abs(to_cell.__x2 - to_cell.__x1) // 2
+        xcenter2 = to_cell.__x1 + xoffset2
+        
+        yoffset2 = abs(to_cell.__y2 - to_cell.__y1) // 2
+        ycenter2 = to_cell.__y1 + yoffset2
+        
+        # if we are undoing, set the line to gray
         if undo:
             fill_color = "gray"
+        else:
+            fill_color = "red"
+            
+        #! Define the center points of each cell in coordinates
+        point1 = Point(xcenter, ycenter)
+        point2 = Point(xcenter2, ycenter2)
+        
+        #! Draw a line between the 2 center points
+        line = Line(point1, point2)
+        self.__window.draw_line(line, fill_color)
 
-        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
-        self.__win.draw_line(line, fill_color)
+        
+        
+        
+        
